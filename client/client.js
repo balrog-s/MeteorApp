f262c70e445b37fd16ca44f463ba4d4281c8ce90
Balraj Shah <BalrajShah@live.com>
2016-01-22 10:39:03 -0500

    Button that allows user to open or close chat

--- client/client.js
+++ client/client.js
@@ -1,10 +1,18 @@
 if(Meteor.isClient){
+
+  //reactive variable for determining whether chat is open or not. Initialized to a closed chat.
+  Session.set('isChatOpen', false);
+
   //finds all the messages in the Collection and sorts them by the most recent message going to the top
   Template.chatBox.helpers({
     messages: function(){
       return Messages.find({}, {sort: {createdAt: -1}});
+    },
+    checkChat: function(){
+      return Session.get('isChatOpen');
     }
   });
+
 
   //this event will be the chatbox available for users of the website
   Template.messagebox.events({
@@ -22,6 +30,14 @@ if(Meteor.isClient){
 
       //reset the value in the messagebox
       event.target.mesg.value="";
+    }
+  });
+
+  //Button that toggles whether chat is open or closed.
+  Template.chatButton.events({
+    'submit .chatOpen': function(event){
+      event.preventDefault();
+      Session.set('isChatOpen', !(Session.get('isChatOpen')));
     }
   });
 
