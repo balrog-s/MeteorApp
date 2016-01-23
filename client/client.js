7167f4382476a4be21e3615a073ac4e6925f3484
Balraj Shah <BalrajShah@live.com>
2016-01-22 23:24:54 -0500

    Added css formatting and placeholder images

--- client/client.js
+++ client/client.js
@@ -1,13 +1,27 @@
+
+
 if(Meteor.isClient){
 
   //reactive variable for determining whether chat is open or not. Initialized to a closed chat.
   Session.set('isChatOpen', false);
 
+  //when the ctrl key is clicked the chat will open
+  Meteor.startup(function () {
+      $(document).on('keyup', function (e) {
+      if(e.keyCode === 17){
+        Session.set('isChatOpen', !Session.get('isChatOpen'));
+      }
+    });
+  });
+
   //finds all the messages in the Collection and sorts them by the most recent message going to the top
   Template.chatBox.helpers({
     messages: function(){
       return Messages.find({}, {sort: {createdAt: -1}});
-    },
+    }
+  });
+
+  Template.body.helpers({
     checkChat: function(){
       return Session.get('isChatOpen');
     }
@@ -21,25 +35,20 @@ if(Meteor.isClient){
       event.preventDefault();
       var usermessage = event.target.mesg.value;
 
+      if(usermessage.length != 0){
       //inserting the message specified into the Messages Collection
       Messages.insert({
         body: usermessage,
         sentBy: Meteor.user().username,
         createdAt: new Date()
       });
+    }
 
       //reset the value in the messagebox
       event.target.mesg.value="";
     }
   });
 
-  //Button that toggles whether chat is open or closed.
-  Template.chatButton.events({
-    'submit .chatOpen': function(event){
-      event.preventDefault();
-      Session.set('isChatOpen', !(Session.get('isChatOpen')));
-    }
-  });
 
   //allows users to login without having to use an email
   Accounts.ui.config({
