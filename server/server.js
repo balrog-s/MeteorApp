c35afc11ada9086f4d6a67beaedde00c6829f85d
Balraj Shah <BalrajShah@live.com>
2016-01-22 23:48:21 -0500

    small css changes

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ if(Meteor.isServer){
   //Empty Messages collection adds a system message to the collection
   if(Messages.find({}).count() === 0){
     Messages.insert({
-      body: 'Welcome to MeteorChat',
+      body: 'Welcome to 9Tails!',
       sentBy: 'System',
       createdAt: new Date()
     });
