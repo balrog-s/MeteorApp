fbdf3e213576d419065e0d4c2a8b67f3cb108383
Balraj Shah <BalrajShah@live.com>
2016-01-29 15:52:52 -0500

    Google Maps and Markers integration

--- client/client.js
+++ client/client.js
@@ -1,16 +1,50 @@
 
-
 if(Meteor.isClient){
 
   //reactive variable for determining whether chat is open or not. Initialized to a closed chat.
   Session.set('isChatOpen', false);
-  Session.set('coordinates');
   //when the ctrl key is clicked the chat will open
   Meteor.startup(function () {
+      GoogleMaps.load();
+      $('#chat').hide();
       $(document).on('keyup', function (e) {
       if(e.keyCode === 17){
         Session.set('isChatOpen', !Session.get('isChatOpen'));
+        if(Session.get('isChatOpen') == true){
+          $('#chat').show();
+        }else{
+          $('#chat').hide();
+        }
       }
+    });
+  });
+
+  Template.map.helpers({
+    mapOptions: function() {
+      if (GoogleMaps.loaded()) {
+        return {
+          center: new google.maps.LatLng(43, -79),
+          zoom: 7
+        };
+      }
+    }
+  });
+
+  Template.map.onCreated(function() {
+    GoogleMaps.ready('map', function(map) {
+      Messages.find().observe({
+        added: function(){
+          Messages.find().forEach(function(loc){
+            console.log(loc.latitude + " "+ loc.longitude);
+            var marker = new google.maps.Marker({
+              position: new google.maps.LatLng(loc.latitude, loc.longitude),
+              animation: google.maps.Animation.DROP,
+              map: map.instance,
+              title: loc.sentBy
+            });
+          });
+        }
+      })
     });
   });
 
@@ -23,16 +57,10 @@ if(Meteor.isClient){
 
   Template.body.helpers({
     checkChat: function(){
-      Session.set('coordinates', Geolocation.latLng());
       return Session.get('isChatOpen');
     }
   });
 
-  Template.location.helpers({
-    geolocation: function(){
-      return GLocation.find({});
-    }
-  })
 
   Template.messagebox.helpers({
     geolocationError: function() {
@@ -42,31 +70,30 @@ if(Meteor.isClient){
   });
 
 
+
   //this event will be the chatbox available for users of the website
   Template.messagebox.events({
     'submit .messageContent': function(event){
       //prevent default browser action on submit
       event.preventDefault();
       var usermessage = event.target.mesg.value;
-      var findLat = Session.get('coordinates').lat;
-      var findLongi = Session.get('coordinates').lng;
 
       if(usermessage.length != 0){
       //inserting the message specified into the Messages Collection
+
       Messages.insert({
         body: usermessage,
         sentBy: Meteor.user().username,
         createdAt: new Date(),
+        latitude: Geolocation.latLng().lat,
+        longitude: Geolocation.latLng().lng
       });
-      GLocation.insert({
-        lat: findLat,
-        longi: findLongi
-      });
-      console.log(GLocation.find({}));
-    }
-
-      //reset the value in the messagebox
+            //reset the value in the messagebox
       event.target.mesg.value="";
+
+    }else{
+      alert("Say something..I'm giving up on you..");
+    }
     }
   });
 
